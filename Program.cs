cea6170bfb1c2893276f9296864b591259e21c61
Dypso <dypsok@gmail.com>
2023-09-01 21:02:22 +0200

    Added methods for adding sequences and generating video

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 namespace WhiteboardCodeAnimation
 {
     class Program
     {
+        static List<SequenceData> sequences = new List<SequenceData>();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            AddSequence("text", "This is an example text.");
+            AddSequence("code", "public class MyClass { }");
+            GenerateVideo();
         }
+        
+        public static void AddSequence(string type, object data)
+        {
+            sequences.Add(new SequenceData { Type = type, Data = data });
+        }
+        
+        public static void GenerateVideo()
+        {
+            // Todo* Implement logic to generate video from sequences
+        }
+    }
+    
+    public class SequenceData
+    {
+        public string Type { get; set; }
+        public object Data { get; set; }
     }
 }
